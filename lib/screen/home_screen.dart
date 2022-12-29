034ccda6bd41f4df8d365a3315a141dab302a32f
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2022-12-29 09:32:33 -0500

    Update of project with the package of atomic design

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:atomic_design/atoms/button_text.dart';
 import 'package:atomic_design/atoms/image.dart';
 import 'package:atomic_design/atoms/input_field.dart';
 import 'package:atomic_design/atoms/text.dart';
 import 'package:atomic_design/fundation/color_fundation.dart';
 import 'package:atomic_design/molecules/app_bar.dart';
 import 'package:atomic_design/molecules/nav_bar.dart';
+import 'package:atomic_design/molecules/search_widget.dart';
 import 'package:atomic_design/tokens/colors_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +18,83 @@ class HomeScreen extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: const OurAppBar(
+      appBar: AppBarWidget(
         title: "",
+        actions: [
+          const CircleAvatar(
+            backgroundColor: scale00,
+            backgroundImage: AssetImage("assets/img/Image.png"),
+          ),
+          SizedBox(width: width * 0.03,),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.notification_add_rounded)),
+          SizedBox(width: width * 0.03,),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.add_a_photo_rounded)),
+          SizedBox(width: width * 0.03,),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert_rounded)),
+          SizedBox(width: width * 0.02,),
+        ],
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              H1(text: "Hello World Atomic design"),
-              H2(text: "I am developer videogames and mobile"),
-              ContentText(text: "Hello are Daniel"),
-              OurImage(
-                url: "https://cdn.shopify.com/s/files/1/1447/5176/files/Logo_Barcraft_tees_400.png?v=1576099165",
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            const H1(text: "Hello World Atomic design", fontSizeText: 30,),
+            SizedBox(height: height * 0.014,),
+            const H2(text: "I am developer videogames and mobile", fontSizeText: 20, colorText: statusSuccess,),
+            SizedBox(height: height * 0.014),
+            const ContentText(text: "Hello are Daniel"),
+            SizedBox(height: height * 0.014,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.037, vertical: height * 0.010),
+              child: const OurImage(
+                url: "https://i.pinimg.com/originals/61/89/7f/61897fbab5cf4e726f8743ce8969faa7.gif",
                 boxFit: BoxFit.cover,
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.037, vertical: height * 0.010),
+              child: const OurImage(
+                url: "https://media.tenor.com/CjqZyaNUSlwAAAAd/boxing-tom-and-jerry.gif",
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.037, vertical: height * 0.010),
+              child: const OurImage(
+                url: "https://media.tenor.com/4ppL9Gf9aDsAAAAd/tom-and-jerry-tom.gif",
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.037, vertical: height * 0.010),
+              child: const OurImage(
+                url: "https://thumbs.gfycat.com/GiddyKindheartedChrysomelid-size_restricted.gif",
+              ),
+            ),
+            SizedBox(height: height * 0.020),
+            SearchWidget(
+              hintText: "Search for...",
+              width: width * 0.8,
+              height: height * 0.08,
+            ),
+            InputField(
+              width: double.infinity, 
+              height: height * 0.08, 
+              hintText: "Writing"
+            ),
+            ButtonText(
+              onPressed: (){}, 
+              buttonStyle: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  statusSuccess
+                ),
+              ),
+              child: const Text("Continue"),
+            )
+          ],
         ),
       ),
-      bottomNavigationBar: const OurFooter(
+      bottomNavigationBar: const NavBarWidget(
+
         bottomNavigationBar: [
           BottomNavigationBarItem(icon: Icon(Icons.shop_rounded), label: "Store", backgroundColor: h2Color),
           BottomNavigationBarItem(icon: Icon(Icons.school_rounded), label: "School", backgroundColor: h2Color),
